a96de02dd1a52aeccb957ddc8cd205ca2018a579
Catalin Pit <catalinpit@gmail.com>
2018-08-04 16:59:56 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/ConfigureStore';
+import { addExpense } from './actions/Expenses';
+import { setTextFilter} from './actions/Filters';
+import getVisibleExpenses from './selectors/Expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 599 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 350, createdAt: 100 }));
+store.dispatch(addExpense({ description: 'Rent bill', amount: 2500 }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
